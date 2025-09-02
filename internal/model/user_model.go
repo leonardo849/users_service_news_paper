19960989e6984223847ef03af12f7d09623fac99
leonardo849 <javascriptleonardolucas@gmail.com>
2025-09-01 22:16:24 -0300

    a

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -2,26 +2,25 @@ package model
 
 import (
 	"time"
-	
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UserModel struct {
-	ID           uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Username     string    `gorm:"size:50;unique;not null" json:"username"`
-	Email        string    `gorm:"size:100;unique;not null" json:"email"`
-	Password string    `gorm:"not null" json:"password"`
-	FullName     string    `gorm:"size:100" json:"fullname"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-	Code *int `json:"code"`
-	IsActive     bool `gorm:"default:false" json:"is_active"`
-	Role string `gorm:"default:'CUSTOMER';not null"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Username  string    `gorm:"size:50;unique;not null" json:"username"`
+	Email     string    `gorm:"size:100;unique;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"password"`
+	FullName  string    `gorm:"size:100" json:"fullname"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Code      *int      `json:"code"`
+	IsActive  bool      `gorm:"default:false" json:"is_active"`
+	Role      string    `gorm:"default:'CUSTOMER';not null"`
 }
 
 func (u *UserModel) BeforeCreate(tx *gorm.DB) (err error) {
 	u.ID = uuid.New()
- 	return
+	return
 }
-
